6a947f8e6404f7c42f42816e816d2e45e99a0ced
Сергей Михайлов <sergeymikhailov@macbook-pro-sergey.local>
2018-08-30 19:45:16 +0300

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Sergey Mikhailov on 30.08.2018.
+//  Copyright © 2018 Sergey Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
